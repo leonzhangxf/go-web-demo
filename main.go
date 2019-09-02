ad30a71d73fb42f03578fd00e53c31b52b0997be
leonzhangxf <leon_zhangxf@qq.com>
2019-09-02 18:29:52 +0800

    + Initial commit.

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("leonzhangxf api start")
+
+	engine := gin.Default()
+
+	engine.GET("/", func(context *gin.Context) {
+		context.String(200, "%s", "leon")
+	})
+
+	_ = engine.Run()
+}
